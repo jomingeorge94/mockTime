c649c385df74a176123605dbff5f2eccea44bf12
jomingeorge94 <j.george2@newcastle.ac.uk>
2016-03-05 10:22:40 +0000

    Admin Side navigation structure

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -70,50 +70,7 @@ if(($user_data['admin_password_check']) == 1){
         </nav>
       </header>
       <!-- Left side column. contains the logo and sidebar -->
-      <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-          <!-- Sidebar user panel -->
-          <div class="user-panel">
-            <div class="pull-left image">
-              <?php echo '<img src="../', $user_data['profile_picture'], '" alt="', $user_data['first_name'], '\'s Profile Picture" class="img-circle" /> '; ?>        
-            </div>
-            <div class="pull-left info">
-              <p><?php echo $user_data['first_name'];?>&nbsp;<?php echo $user_data['last_name'];?></p>
-              <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-            </div>
-          </div>
-          <!-- search form -->
-          <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-              <input type="text" name="q" class="form-control" placeholder="Search...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
-              </span>
-            </div>
-          </form>
-          <!-- /.search form -->
-          <!-- sidebar menu: : style can be found in sidebar.less -->
-          <ul class="sidebar-menu">
-            <li class="header">MAIN NAVIGATION</li>
-            <li class="active treeview">
-              <a href="#">
-                <i class="fa fa-dashboard"></i> <span>Dashboard</span> 
-              </a>
-            </li>
-
-
-            <li class="header">Manage</li>
-            <li><a href="#"><i class="fa fa-cogs"></i> <span>Category Listing</span></a></li>
-            <li><a href="#"><i class="fa fa-plus-circle"></i> <span>Add Category</span></a></li>
-            <li><a href="#"><i class="fa fa-cog"></i> <span>Exam Listing</span></a></li>
-            <li><a href="#"><i class="fa fa-puzzle-piece"></i> <span>Add Exam</span></a></li>
-
-           
-          </ul>
-        </section>
-        <!-- /.sidebar -->
-      </aside>
+      <?php include '/includes/admin_side_navigation.php';?>
 
       <!-- Content Wrapper. Contains page content -->
       <div class="content-wrapper">
--- admin/admin_dashboard_usersregisteres.php
+++ admin/admin_dashboard_usersregisteres.php
@@ -66,42 +66,7 @@
       </nav>
     </header>
       <!-- Left side column. contains the logo and sidebar -->
-    <aside class="main-sidebar">
-      <!-- sidebar: style can be found in sidebar.less -->
-      <section class="sidebar">
-        <!-- Sidebar user panel -->
-        <div class="user-panel">
-          <div class="pull-left image">
-            <?php echo '<img src="../', $user_data['profile_picture'], '" alt="', $user_data['first_name'], '\'s Profile Picture" class="img-circle" /> '; ?>        
-          </div>
-          <div class="pull-left info">
-            <p><?php echo $user_data['first_name'];?>&nbsp;<?php echo $user_data['last_name'];?></p>
-            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-          </div>
-        </div>
-
-        <!-- search form -->
-        <form action="#" method="get" class="sidebar-form">
-          <div class="input-group">
-            <input type="text" name="q" class="form-control" placeholder="Search...">
-            <span class="input-group-btn">
-              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
-            </span>
-          </div>
-        </form>
-
-        <ul class="sidebar-menu">
-          <li class="header">MAIN NAVIGATION</li>
-          <li class="active treeview">
-            <a href="#">
-              <i class="fa fa-dashboard"></i> <span><a href="admin_dashboard.php">Dashboard</a></span> 
-            </a>
-
-          </li>
-         
-        </ul>
-      </section>
-    </aside>
+    <?php include '/includes/admin_side_navigation.php';?>
 
     <div class="content-wrapper">
       <section class="content-header">
